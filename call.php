9d853f05323076feaa1a0d0891fd90e8b6c2e3a3
journey6 <blackflower24@gmail.com>
2019-04-20 01:58:41 +0300

    Update call.php

--- call.php
+++ call.php
@@ -1,20 +1,11 @@
 <?PHP
-// set the default timezone to use. Available since PHP 5.1
-date_default_timezone_set('Asia/Riyadh');
-
-$dateTime = date("Y-m-d H:i:s");
-$today = date("Y-m-d");
-$std_civil = null;
-$cardID = null;
 
 
  if(!empty($_POST['cardID'])){
 	 
-	 
-
 	 if($_POST['cardID'] == 1){
 		//echo "CARD ID: ".$_POST['cardID'];
-		echo "Name: abdullah"."\n";
+		echo "Name: Tony"."\n";
 	 }else{
 		//echo "CARD ID: ".$_POST['cardID'];
 		echo "Name: Unknown user"."\n";
